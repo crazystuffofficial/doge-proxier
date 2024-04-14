c95c473700e0f2d4b6b3ddac8a69e256294c1817
Crazystuffxyz <125907466+crazystuffofficial@users.noreply.github.com>
2024-04-14 12:00:40 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const proxyChecker = require('proxy-checker');
+const express = require('express');
+const request = require('request');
+
+function createDoge(proxy, port){
+  
+}
+https://www.proxy-list.download/api/v1/get?type=http
